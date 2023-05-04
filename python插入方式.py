bce96149ad9d884eb0957c5a55154826dcfcbb05
hansheng-zhang <1718810184@qq.com>
2023-05-04 13:32:44 +0800

    更新 python 插入方式及改名

--- python插入方式.py
+++ python插入方式.py
@@ -1,15 +1,71 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# 读取Excel文件
-file_path = '大学相关/专业.xlsx'
-sheet_name = 'Sheet1'
-df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
-
-# 创建一个到数据库的连接（Mac使用的语句，不知道windows是否相同，root后要填密码，我的没有填）
+# Connect to database
 db_connection_string = "mysql+pymysql://root:@localhost:3306/Eduspark"
 engine = create_engine(db_connection_string)
 
-# 将数据插入SQL表格
+
+# 1. University
+file_path = 'University/University.xlsx'
+sheet_name = 'Sheet1'
+df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+
+table_name = 'university'
+df.to_sql(table_name, engine, if_exists='append', index=False)
+
+# 2. University_major
+file_path = 'University/Major.xlsx'
+sheet_name = 'Sheet1'
+df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+
 table_name = 'uni_maj'
+df.to_sql(table_name, engine, if_exists='append', index=False)
+
+# 3. Undergraduate university
+file_path = 'Application/Undergraduate university.xlsx'
+sheet_name = 'Sheet1'
+df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+
+table_name = 'undergra_univers'
+df.to_sql(table_name, engine, if_exists='append', index=False)
+
+# 4. Appliers
+file_path = 'Application/Appliers.xlsx'
+sheet_name = 'Sheet1'
+df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+
+table_name = 'appliers'
+df.to_sql(table_name, engine, if_exists='append', index=False)
+
+# 5. Applications
+file_path = 'Application/Applications.xlsx'
+sheet_name = 'Sheet1'
+df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+
+table_name = 'applications'
+df.to_sql(table_name, engine, if_exists='append', index=False)
+
+# 6. Programs
+file_path = 'Program/Program.xlsx'
+sheet_name = 'Sheet1'
+df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+
+table_name = 'program'
+df.to_sql(table_name, engine, if_exists='append', index=False)
+
+# 7. Employment
+file_path = 'Program/Employment.xlsx'
+sheet_name = 'Sheet1'
+df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+
+table_name = 'employment'
+df.to_sql(table_name, engine, if_exists='append', index=False)
+
+# 8. Standardized test
+file_path = 'Program/Standardized test.xlsx'
+sheet_name = 'Sheet1'
+df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+
+table_name = 'standardized_test'
 df.to_sql(table_name, engine, if_exists='append', index=False)
